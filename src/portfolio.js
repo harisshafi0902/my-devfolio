a0528d3f9f8b66cbe69b2f367da26e0d43b3db73
harisshafi0902 <theharisshafi@gmail.com>
2024-06-20 12:41:31 +0500

    complete dev folio

---
+++ src/portfolio.js
@@ -0,0 +1,241 @@
+
+
+export const greetings = {
+  name: 'Haris Shafi',
+  title: "Hi, I'm",
+  role: 'MERN/MEAN Developer',
+  info: 'I transforms the UI designs into high-quality code & develop responsive, optimized & efficient websites',
+  description: [
+    "I'm Haris Shafi, a detail-oriented & creative full-stack developer who worked well in a team or independently to applications primarliy developed using MERN/MEAN. I like to create or transform the UI's or wireframes into functional & responsive site as well as built scalable web applications",
+    'Seeking a position in a firm where i can utilize my knowledge & skills, that not only makes contribution to the firm but also lead to the enhancement of my current skillset & to get better exposure to the field.',
+  ],
+  profileImg: 'https://media.licdn.com/dms/image/D4D03AQGa6EP3iJH5zA/profile-displayphoto-shrink_400_400/0/1697040168364?e=1724284800&v=beta&t=dXRaSMJFGzjDIeQs1_jU96ByRPkF_2PofZGed8nYMdw',
+};
+
+export const openSource = {
+  githubUserName: 'harisshafi0902',
+};
+
+export const contact = {
+  email: 'theharisshafi@gmail.com',
+};
+
+export const socialLinks = {
+  github: 'https://github.com/harisshafi0902',
+  linkedin: 'https://www.linkedin.com/in/haris-shafi',
+  resume:
+    'https://drive.google.com/file/d/1VYid2VCo5SHbP2CfsjuYUiACRm4_Wr4n/view?usp=sharing',
+};
+
+export const skillSet = [
+  {
+    skillName: 'Reactjs',
+    fontAwesomeClassname: 'vscode-icons:file-type-reactjs',
+  },
+  {
+    skillName: 'Nodejs',
+    fontAwesomeClassname: 'logos:nodejs-icon',
+  },
+  {
+    skillName: 'ExpressJS',
+    fontAwesomeClassname: 'logos:express',
+  },
+  {
+    skillName: 'MongoDB',
+    fontAwesomeClassname: 'logos:mongodb',
+  },
+  {
+    skillName: 'Angular',
+    fontAwesomeClassname: 'logos:angular',
+  },
+  {
+    skillName: 'MySql',
+    fontAwesomeClassname: 'logos:mysql',
+  },
+  {
+    skillName: 'Jest',
+    fontAwesomeClassname: 'logos:jest',
+  },
+  {
+    skillName: 'Docker',
+    fontAwesomeClassname: 'logos:docker',
+  },
+  {
+    skillName: 'Linux',
+    fontAwesomeClassname: 'logos:linux-tux',
+  },
+  {
+    skillName: 'JavaScript',
+    fontAwesomeClassname: 'logos:javascript',
+  },
+  {
+    skillName: 'TypeScript',
+    fontAwesomeClassname: 'logos:typescript-icon',
+  },
+  {
+    skillName: 'Git',
+    fontAwesomeClassname: 'logos:git-icon',
+  },
+  {
+    skillName: 'Material UI',
+    fontAwesomeClassname: 'simple-icons:mui',
+  },
+  {
+    skillName: 'HTML5',
+    fontAwesomeClassname: 'vscode-icons:file-type-html',
+  },
+  {
+    skillName: 'CSS3',
+    fontAwesomeClassname: 'vscode-icons:file-type-css',
+  },
+  {
+    skillName: 'Jira',
+    fontAwesomeClassname: 'logos:jira',
+  },
+];
+
+export const experience = [
+  {
+    role: 'Software Engineer',
+    company: 'Softoo Ltd',
+    companylogo: 'https://softoo.co/images/Softoo-main-logo.svg',
+    date: 'Jan 2024 – Present',
+    descBullets: [
+      'Development of Full-Stack applications, implementing key functionalities such as security, authentication, error handling, file handling, & payment processes',
+      'Responsibly worked on implementing good coding practices, ensuring both functionality & security of the application',
+    ],
+  },
+  {
+    role: 'Junior Software Engineer',
+    company: 'SOFTOO',
+    // companylogo: '/img/icons/common/google.svg',
+    date: 'Jan 2023 – Dec 2023',
+    descBullets: [
+      'Developing & implementing highly-responsive user interface using React & Node.js concepts, translating designs into interactive, pixel-perfect, & responsive full-stack applications.',
+      'Integrating APIs, managing data in the front-end codebase with MySQL queries, & writing comprehensive Jest test cases to ensure functionality & reliability.'
+    ],
+  },
+  {
+    role: 'MERN/MEAN Lab Assistant WEEKEND',
+    company: 'Softoo / P@sha / PSEB',
+    // companylogo: '/img/icons/common/google.svg',
+    date: 'Jan 2023 – Aug 2023 Weekend Program',
+    descBullets: [
+      'Responsible for delivering lectures & leading hands-on exercises for the bootcamp students.',
+      'Assigning tasks to students for their development practices.'
+    ],
+    
+  },
+  {
+    role: 'React Developer',
+    company: 'Nayatel',
+    // companylogo: '/img/icons/common/google.svg',
+    date: 'July 2022 – Dec 2022',
+    descBullets: [
+      'Implementing highly-responsive user interface components using React & translating designs to interactive, pixel perfect & responsive sites',
+    ],
+  },
+];
+
+export const projects = {
+  collaboration: [
+    {
+      projectType: 'consultations & quotes for builder services',
+      name: 'EASEFIX',
+      desc: 'Book a Guaranteed Tradesperson and Discover 60000 reliable UK Tradespersons – builders, electricians,',
+      link: 'https://example.com',
+      techList: [
+        'Angular',
+        'NodeJS',
+        'MongoDB',
+        'Modules',
+        'Directories',
+       ' ng e2e',
+        'angular/cli',
+        'Karma',
+        'ngx',
+
+
+      ],
+      status: 'completed',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShqSQrirJ710QbKycuKUweSGdmwn5Sa5Y3hw&s',
+      extras: {
+        color: '#46B9F6',
+        screenshot: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXXmNaSNZraQAS7WmMIkqGN_xrUWfmDmqKkQ&s',
+      },
+    },
+    {
+      projectType: 'Online fast-fashion retailer',
+      name: 'BOOHOO',
+      desc: "Shop boohoo's range of women's and men's clothing for the latest fashion pieces",
+      link: 'https://example.com',
+      techList: [
+        'React JS',
+        'Node JS',
+        'Material-UI',
+        'Typescript',
+        'Yup',
+        'Jest',
+        'React Hook Form',
+      ],
+      logo: 'https://innovation-central.co.uk/wp-content/uploads/2022/03/Boohoo-Logo.png',
+      extras: {
+        color: '#ff679b',
+        screenshot: 'https://econsultancy.imgix.net/content/uploads/2020/05/14160211/boohoo-Facebook-page.jpg',
+        status: 'In Development',
+      },
+    },
+    {
+      projectType: 'Smart Media Box ',
+      name: 'Nayatel Smart Box',
+      desc: 'An auction marketplace where you can propose the buying & selling of any item or experience you want that you cannot get normally.',
+      link: 'https://example.com',
+      techList: ['React JS', 'Node JS', 'Material-UI', 'Axios', 'Cors', 'MySql', 'Typescript', 'Jest', 'React Hook Form',],
+      logo: 'https://nayatel.com/wp-content/uploads/2019/11/Layer-3.png',
+      extras: {
+        color: '#7c7c7c',
+        screenshot: 'https://nayatel.com/wp-content/uploads/2024/01/press-release-ATV-banner.png',
+      },
+    },
+  ],
+  personal: [
+    {
+      name: 'Amazon Clone App (MERN | TS)',
+      desc: ' Fully-functional E-commerce website exactly like amazon. Build an E-commerce website using MERN stack (MongoDB, ExpressJS, React and Node.JS).',
+      repoLink: 'https://github.com/harisshafi0902/MERN-ECOMMERCE',
+      techList: [
+        'React JS',
+        'TypeScript',
+        'NodeJs',
+        'ExpressJS',
+        'MongoDB',
+        'Redux-Toolkit',
+        'Joi',
+        'Axios',
+      ],
+    },
+    {
+      name: 'Travel World Website (MERN | TS)',
+      desc: 'Travel website for complete places info along with SignIn & SignUp functionality.',
+      repoLink: 'https://github.com/harisshafi0902/Mern-Travelling-Website',
+      techList: ['React JS', 'Bootstrap', 'NodeJs','TypeScript',
+        
+        'ExpressJS',
+        'MongoDB', 'Bcrypt', 'Axios'],
+    },
+    {
+      name: 'SWAGGER (Alternate Postman)',
+      desc: 'Simplify your API development with our open-source and professional tools, built to help you and your team efficiently design and document APIs at scale.',
+      repoLink: 'https://github.com/harisshafi0902/node-swagger',
+      techList: [
+        'cors',
+        'express',
+        'mysql',
+        'nodemon',
+        'swagger-jsdoc',
+        'swagger-ui-express',
+
+      ],
+    },
+  ],
+};
